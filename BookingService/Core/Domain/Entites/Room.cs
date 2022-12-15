e587ba34e7ea1afdffc1814c64e7c2393c8eba7a
Vitor Alves <vitormanoel00710@gmail.com>
2022-12-15 16:31:26 -0300

    Adicionar arquivos de projeto.

---
+++ BookingService/Core/Domain/Entites/Room.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entites
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintnance { get; set; }
+        public bool IsAvailable
+        {
+            get
+            {
+                if (this.InMaintnance || this.HasGuest) return false;
+                return true;
+            }
+        }
+        public bool HasGuest
+        {
+            // Verify if exists open bookings to this room
+            get { return true;  }
+        }
+    }
+}
---
+++ BookingService/Tests/Domain/DomainTests/Booking/BookingTests.cs
@@ -0,0 +1,82 @@
+﻿using Domain.Entites;
+using Domain.Enuns;
+using NUnit.Framework;
+using Action = Domain.Enuns.Action;
+
+namespace DomainTests.Bookings
+{
+    public class BookingTests
+    {
+        [Test]
+        public void ShouldAlwaysStartWithCreatedStatus()
+        {
+            var booking = new Booking();
+            Assert.AreEqual(booking.CurrentStatus, Status.Created);
+        }
+        
+        [Test]
+        public void ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(Action.Pay);
+
+            Assert.AreEqual(booking.CurrentStatus, Status.Paid);
+        }
+        
+        [Test]
+        public void ShouldSetStatusToCanceledWhenCancelingForABookingWithCreatedStatus()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(Action.Cancel);
+
+            Assert.AreEqual(booking.CurrentStatus, Status.Canceled);
+        }
+        
+        [Test]
+        public void ShouldSetStatusToFinishedWhenFinishingForAPaidBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(Action.Pay);
+            booking.ChangeState(Action.Finish);
+
+            Assert.AreEqual(booking.CurrentStatus, Status.Finished);
+        }
+        
+        [Test]
+        public void ShouldSetStatusToRefoundWhenRefoundingForAPaidBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(Action.Pay);
+            booking.ChangeState(Action.Refound);
+
+            Assert.AreEqual(booking.CurrentStatus, Status.Refounded);
+        }
+        
+        [Test]
+        public void ShouldSetStatusToCreatedWhenReopeningForACanceledBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(Action.Cancel);
+            booking.ChangeState(Action.Reopen);
+
+            Assert.AreEqual(booking.CurrentStatus, Status.Created);
+        }
+        
+        [Test]
+        public void ShouldNotChangeStatusWhenRefoundingAFinishedBooking()
+        {
+            var booking = new Booking();
+
+            booking.ChangeState(Action.Pay);
+            booking.ChangeState(Action.Finish);
+            booking.ChangeState(Action.Refound);
+
+            Assert.AreEqual(booking.CurrentStatus, Status.Finished);
+        }
+    }
+}
